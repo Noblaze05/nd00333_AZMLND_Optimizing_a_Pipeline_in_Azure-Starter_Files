ac8ff30b62382a4900b65332abe2b534b83e0a1e
Noblaze05 <gildasbarraud@gmail.com>
2021-02-11 22:09:14 +0000

    training 1st success

--- train.py
+++ train.py
@@ -15,13 +15,6 @@ web_url = "https://automlsamplenotebookdata.blob.core.windows.net/automl-sample-
 
 ds = TabularDatasetFactory.from_delimited_files(web_url)
 
-x, y = clean_data(ds)
-
-# Split data into train and test sets.
-x_train, x_train, y_train, y_test = train_test_split(x, y, test_size=0.33)
-
-run = Run.get_context()
-
 def clean_data(data):
     # Dict for cleaning data
     months = {"jan":1, "feb":2, "mar":3, "apr":4, "may":5, "jun":6, "jul":7, "aug":8, "sep":9, "oct":10, "nov":11, "dec":12}
@@ -48,6 +41,14 @@ def clean_data(data):
 
     y_df = x_df.pop("y").apply(lambda s: 1 if s == "yes" else 0)
     
+    return x_df, y_df
+
+x, y = clean_data(ds)
+
+# Split data into train and test sets.
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.33)
+
+run = Run.get_context()
 
 def main():
     # Add arguments to script
